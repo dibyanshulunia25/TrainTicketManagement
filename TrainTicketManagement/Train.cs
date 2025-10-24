cc73a947045da4cc7c098db007d27c79a29203af
Atul singh <atusingh1618@gmail.com>
2025-10-24 21:57:59 +0530

    minor bug fixes

--- TrainTicketManagement/Train.cs
+++ TrainTicketManagement/Train.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TrainTicketManagement
 {
-    public partial class Form1 : Form
+    public partial class Train : Form
     {
-        public Form1()
+        public Train()
         {
             InitializeComponent();
         }
